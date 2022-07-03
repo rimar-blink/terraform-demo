4ccb6b8db94eec05be59eb815ca562fcb8cc36fa
Yuval <yuval@blinkops.com>
2022-07-03 13:53:17 +0300

    add s3 bucket provisioning with parameters

--- s3/main.tf
+++ s3/main.tf
@@ -1,11 +1,21 @@
+variable "bucket_name" {
+    type = string
+    description = "Bucket name"
+}
+
+variable "region" {
+    type = string
+    description = "AWS region"
+}
+
+
 provider "aws" {
-    region = "eu-west-2"
+    region = var.region
 }
 
 resource "aws_s3_bucket" "demos3" {
-    bucket = "yuval-demo-bucket-1" 
+    bucket = var.bucket_name
 }
-
 
 resource "aws_s3_bucket_acl" "example_bucket_acl" {
   bucket = aws_s3_bucket.demos3.id
